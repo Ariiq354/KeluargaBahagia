6cc67002b388c713c015cb5beaba6ad5b2cf3beb
n00b St4r <realgoldcraft@gmail.com>
2024-05-06 12:45:38 +0700

    chore: change db to turso

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,11 @@
 import type { Config } from 'drizzle-kit';
 export default {
   schema: './src/lib/server/schema.ts',
-  driver: 'libsql',
+  driver: 'turso',
   out: './drizzle',
   dbCredentials: {
-    url: 'file:test.db'
+    url: process.env.DATABASE_URL ?? '',
+    authToken: process.env.DATABASE_AUTH_TOKEN ?? ''
   },
   verbose: true
 } satisfies Config;
--- src/lib/server/index.ts
+++ src/lib/server/index.ts
@@ -1,10 +1,12 @@
 import { DrizzleSQLiteAdapter } from '@lucia-auth/adapter-drizzle';
 import { drizzle } from 'drizzle-orm/libsql';
 import { createClient } from '@libsql/client';
+import { DATABASE_URL, DATABASE_AUTH_TOKEN } from '$env/static/private';
 import * as schema from './schema';
 
 const client = createClient({
-  url: 'file:test.db'
+  url: DATABASE_URL,
+  authToken: DATABASE_AUTH_TOKEN
 });
 
 export const db = drizzle(client, { schema });
